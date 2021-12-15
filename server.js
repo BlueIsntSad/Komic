35c6313ca8aabebdf213e95db553f11369c6a9fb
Koi <trongkhoi215@gmail.com>
2021-12-16 01:02:45 +0700

    404

--- server.js
+++ server.js
@@ -67,13 +67,11 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 // Express session
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  })
-);
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -97,6 +95,8 @@ app.use('/admin', adminRouter)
 app.use('/user', userRoute)
 // Log in/ Register
 app.use('/', authRoute);
+
+app.use((req, res, next) => { res.status(404).render('error', { layout: false }) });
 
 // Listen request
 app.listen(process.env.PORT || port, function () {
