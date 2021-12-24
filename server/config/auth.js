60eadf226b83a7b022f7ae64a229df85efcad36f
qtuong1729 <62126447+qtuong1729@users.noreply.github.com>
2021-12-24 16:50:55 +0700

    forward Storage

--- server/config/auth.js
+++ server/config/auth.js
@@ -24,6 +24,18 @@ module.exports = {
       req.flash('error_msg', 'Đăng nhập để tiếp tục');
       res.redirect('/login');
     },
+    forwardStorage: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        if (req.user.name == 'admin_test'){
+          return res.redirect('/admin');
+        }
+        return res.redirect('/user/' + req.user.id +'/storage');
+        /*if ( req.params.uid === req.user.id) {return next();}
+        else res.redirect('/user')*/
+      }
+      req.flash('error_msg', 'Đăng nhập để tiếp tục');
+      res.redirect('/login');
+    },
     forwardAuthenticated: function(req, res, next) {
       if (!req.isAuthenticated()) {
         return next();
